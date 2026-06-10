5a06b3956820e876395ca2f7a2d96e084765499a
mmilince <mmilince@gmail.com>
2026-06-10 11:39:00 +0200

    Add altpgen script

---
+++ Tasks/Task3/altpgen.ps1
@@ -0,0 +1,10 @@
+﻿cd C:\Users\adr21\.vscode\extensions\ms-dynamics-smb.al-18.0.2293710\bin\win32\altpgen
+
+.\altpgen.exe `
+ -project:"C:\Projects\Conference\2026\BCTechDays\IntegrateBusinessCentralWithPowerPlatform" `
+ -packagecachepath:"C:\Projects\Conference\2026\BCTechDays\IntegrateBusinessCentralWithPowerPlatform\.alpackages" `
+ -serviceuri:"https://orgfe9fa347.crm11.dynamics.com" `
+ -entities:crf83_product `
+ -baseid:50120 `
+ -clientid:"afac9646-0a94-4ef7-ad1e-4372e76843e4" `
+ -redirecturi:"msalafac9646-0a94-4ef7-ad1e-4372e76843e4://auth"
